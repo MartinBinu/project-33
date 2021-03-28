f20ac547012c011d3749f240dea4d717d86fa305
MartinBinu <68335599+MartinBinu@users.noreply.github.com>
2021-03-28 18:23:57 +0530

    Add files via upload

--- js/Particle2.js
+++ js/Particle2.js
@@ -1,5 +1,5 @@
 class Particle2 {
-   constructor(x, y,radius) {
+   constructor(x,y,radius) {
 
       var options ={
           "restitution":0.4,
@@ -7,15 +7,14 @@ class Particle2 {
           "density":1.0
       }
       
-      this.body = Bodies.circle(x, y,radius,options);      
+      this.body = Bodies.circle(x,y,radius,options);     
       this.radius = radius; 
-      this.body.position.x = mouseX;
       this.color = color(random(0, 255), random(0, 255), random(0, 255));
       World.add(world, this.body);
 
    }
 
-   display() {
+   display(){
       var pos = this.body.position;
       var angle = this.body.angle;
 
@@ -29,12 +28,5 @@ class Particle2 {
       ellipse(0, 0, this.radius,this.radius);
       pop();
    }
-
-   controller(){
-      this.body.position.x = mouseX;
-   }
-
-   noControl(){
-      this.body.position.x = null;
-   }
+   
 };
